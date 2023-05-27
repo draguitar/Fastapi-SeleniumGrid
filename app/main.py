f7b6cde5a48d5c141097a804f00ab74660a09af2
LongerLung <draguitar@gmail.com>
2023-05-27 08:14:59 +0000

    First commit

---
+++ app/main.py
@@ -0,0 +1,47 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+from selenium import webdriver
+
+import time
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World",
+    "Amber": "!!!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/selenium/{item_id}")
+def selenium(item_id: int):
+    try:
+        browser = webdriver.Remote(
+        command_executor='http://10.14.140.15:14444/wd/hub',
+        options=webdriver.ChromeOptions()
+        )
+        browser.get('https://www.google.com')
+        browser.save_screenshot("./app/chrome.png")
+        print(browser.title)
+
+
+    except Exception as error:
+        print(error)
+    finally:
+        time.sleep(30)
+        browser.quit()
+
+
+    return {"item_id": item_id}
+
+
+
+
+
